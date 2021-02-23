6bed068ddf0605a5e9345e94a2b9bdc7caf9e99e
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 00:31:11 -0600

    Render activity item each activity service

--- src/app/components/activity-list/activity-list.component.ts
+++ src/app/components/activity-list/activity-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-activity-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-list.component.scss']
 })
 export class ActivityListComponent implements OnInit {
+
+  @Input() activities: any[]; 
 
   constructor() { }
 
