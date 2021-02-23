cab6b6159b039ed7f947f39725fab6beddccc831
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 03:47:27 -0600

    Consumed events components childs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { AfterContentChecked, Component } from '@angular/core';
+import { AfterContentChecked, Component, ViewChild } from '@angular/core';
 import { Activity } from './models/activity';
 import { ActivitiesService } from './services/activities.service';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +13,38 @@ export class AppComponent implements AfterContentChecked {
   activitiesService: Activity[];
   version: string;
 
+  @ViewChild('register', { static: true }) registerEl: RegisterComponent;
+
   constructor(private activityService: ActivitiesService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.activityService.searchDataStorage();
   }
 
-  ngAfterContentChecked() {
-    this.activitiesService = this.activityService.getAllActivities();
+  ngAfterContentChecked(): void {
+    this.activitiesService = this.activityService.getAllActivities()
+      .reduce( (p, c) => {
+        p = [...p, {...c, register: this.activityService.timeToString(c.register), lastUpdate: c.hasOwnProperty('lastUpdate') ? this.activityService.timeToString(c.lastUpdate) : undefined}]
+        return p;
+      }, []);
     this.version = this.activityService.lastVersion;
   }
 
-  deleteData() {
+  deleteData(): void {
     this.activityService.deleteDataStorage();
+  }
+
+  registerActivity({ name, description }): void {
+    if (this.activityService.existNameActivity(name)) {
+      alert('Intento de Registro de Actividad Repetido');
+      return;
+    }
+    this.activityService.registerActivity(name, description);
+    this.registerEl.cleanControls();
+  }
+
+  updateStatus({ id, status }): void {
+    this.activityService.updateActivity(id, status);
   }
 
 }
