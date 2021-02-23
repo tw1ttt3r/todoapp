82e0f8c5f3d0a6bdd68cfeae9230861f724b53d6
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 00:30:25 -0600

    Get all activities from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component } from '@angular/core';
+import { ActivitiesService } from './services/activities.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
 
+  activitiesService: any[];
+
+  constructor(private activityService: ActivitiesService) {}
+
+  ngOnInit() { }
+
+  ngAfterContentChecked() {
+    this.activitiesService = this.activityService.getAllActivities();
+  }
 }
