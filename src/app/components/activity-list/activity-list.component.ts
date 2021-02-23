2ffb68a9588aa54170279779905a59ea74e991a8
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 03:46:42 -0600

    Emit change status data and delete localStorage data

--- src/app/components/activity-list/activity-list.component.ts
+++ src/app/components/activity-list/activity-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Activity } from 'src/app/models/activity';
+import { Activity } from '../../models/activity';
 
 @Component({
   selector: 'app-activity-list',
@@ -11,14 +11,19 @@ export class ActivityListComponent implements OnInit {
   @Input() activities: Activity[];
   @Input() version: string;
   @Output() emptyData = new EventEmitter();
+  @Output() itemChangeStatus = new EventEmitter<{ id: number, status: number }>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  empty() {
+  empty(): void {
     this.emptyData.emit();
+  }
+
+  statusChange({ id, status }): void {
+    this.itemChangeStatus.emit({ id, status });
   }
 
 }
