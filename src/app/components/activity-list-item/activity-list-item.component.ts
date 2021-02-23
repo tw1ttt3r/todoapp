e9509f4da6f9bc728ee3e0f615011e7369d7567e
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 02:09:38 -0600

    Implemented detail dates activities

--- src/app/components/activity-list-item/activity-list-item.component.ts
+++ src/app/components/activity-list-item/activity-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MonthsName } from 'src/app/enums/months-name.enum';
 import { StatusActivity } from 'src/app/enums/status-activity.enum';
 import { Activity } from 'src/app/models/activity';
 import { ActivitiesService } from 'src/app/services/activities.service';
@@ -12,6 +13,7 @@ export class ActivityListItemComponent implements OnInit {
 
   @Input() activity: Activity;
   statusActivity = StatusActivity
+  monthsName = MonthsName;
 
   constructor(private activityService: ActivitiesService) { }
 
@@ -20,6 +22,11 @@ export class ActivityListItemComponent implements OnInit {
 
   changeStatus(id: number, status: number) {
     this.activityService.updateActivity(id, status);
+  }
+
+  getDateRegistro(time: number) {
+    const fecha = new Date(time);
+    return `${fecha.getDate()}-${this.monthsName[fecha.getMonth()]}-${fecha.getFullYear()} a las ${fecha.getHours() < 12 ? `0${fecha.getHours()}` : fecha.getHours()}:${fecha.getMinutes() < 10 ? `0${fecha.getMinutes()}` : fecha.getMinutes()}:${fecha.getSeconds() < 10 ? `${fecha.getSeconds()}` : fecha.getSeconds() } ${fecha.getHours() < 12 ? `am` : `pm`}`
   }
 
 }
