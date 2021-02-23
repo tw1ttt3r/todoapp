f25d2b8d2878b01e8109b756818057e89a088735
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 03:05:38 -0600

    Added methods localStorage manage

--- src/app/services/activities.service.ts
+++ src/app/services/activities.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Activity } from '../models/activity';
+import { MonthsName } from 'src/app/enums/months-name.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,9 @@ import { Activity } from '../models/activity';
 export class ActivitiesService {
 
   activities: Activity[] = [];
+  lastUpdate: number = 0;
+  monthsName = MonthsName;
+  db = 'todoAppData';
 
   constructor() { }
 
@@ -21,6 +25,7 @@ export class ActivitiesService {
         register: Date.now(),
       }
     ];
+    this.saveDataStorage();
   }
 
   getAllActivities(): Activity[] {
@@ -29,9 +34,44 @@ export class ActivitiesService {
 
   updateActivity(id: number, status: number) {
     this.activities[id] = { ...this.activities[id], status, lastUpdate: Date.now()}
+    this.saveDataStorage();
   }
 
   existNameActivity(name: string): boolean {
     return this.activities.filter( a => a.name === name).length === 0 ? false : true;
+  }
+
+  searchDataStorage(): void {
+    if (!!localStorage.getItem(this.db)) {
+      const { update, data } = JSON.parse(localStorage.getItem(this.db));
+      this.lastUpdate = update;
+      this.activities = data;
+      console.log('update', update)
+      console.log('data', data)
+    }
+  }
+
+  saveDataStorage() {
+    const update = Date.now();
+    localStorage.setItem(this.db, JSON.stringify({update, data: this.activities}));
+    this.lastUpdate = update;
+  }
+
+  deleteDataStorage() {
+    localStorage.removeItem(this.db);
+    this.activities = [];
+    this.lastUpdate = 0;
+  }
+
+  get lastVersion(): string {
+    if (this.lastUpdate === 0) {
+      return '----'
+    }
+    return this.timeToString(this.lastUpdate);
+  }
+
+  timeToString(time: number) {
+    const fecha = new Date(time);
+    return `${fecha.getDate()}-${this.monthsName[fecha.getMonth()]}-${fecha.getFullYear()} a las ${fecha.getHours() < 12 ? `0${fecha.getHours()}` : fecha.getHours()}:${fecha.getMinutes() < 10 ? `0${fecha.getMinutes()}` : fecha.getMinutes()}:${fecha.getSeconds() < 10 ? `0${fecha.getSeconds()}` : fecha.getSeconds() } ${fecha.getHours() < 12 ? `am` : `pm`}`
   }
 }
