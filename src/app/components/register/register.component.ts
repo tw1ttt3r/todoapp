adcfe68d794d2d20180475511cc177bfecaa1370
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-23 00:02:37 -0600

    Register activities in component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  @ViewChild('nameActivity', { static: true } ) activityName: ElementRef<HTMLInputElement>;
+  @ViewChild('descriptionActivity', { static: true } ) activityDescription: ElementRef<HTMLTextAreaElement>;
+
+  activitiesList: any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.activitiesList = [];
   }
 
   saveActivity() {
-    alert('Guardando actividad');
+    if (this.activityName.nativeElement.value !== '' && this.activityDescription.nativeElement.value != '') {
+      this.activitiesList = [
+        ...this.activitiesList,
+        {
+          name: this.activityName.nativeElement.value,
+          description: this.activityDescription.nativeElement.value,
+          status: 'Pendiente',
+          register: Date.now(),
+          lastUpdate: null
+        }
+      ];
+      this.cleanControls();
+    } else {
+      alert('Datos Faltantes')
+    }
+  }
+
+  cleanControls() {
+    this.activityName.nativeElement.value = '';
+    this.activityDescription.nativeElement.value = '';
   }
 
 }
